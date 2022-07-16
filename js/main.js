4ae96c1336fd52e68c2b9332c459114bc6f3746e
Ignacio Di Paolo <nachobeatbox@gmail.com>
2022-07-16 01:38:53 -0300

    feat: opt imgs & details

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
-    const elevator = new Elevator({
-        element: document.querySelector('#btn-subir'),
-        duration: 1000 // milliseconds
-    });
+    // const elevator = new Elevator({
+    //     element: document.querySelector('#btn-subir'),
+    //     duration: 1000 // milliseconds
+    // });
 }
